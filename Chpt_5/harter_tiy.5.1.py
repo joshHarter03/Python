21fe51ace14234aac57599a5df05c37fa689b115
Paxton <joshua.harter@fwcsstudents.org>
2019-10-22 11:26:43 -0400

    Chapter 5 uploaded

---
+++ Chpt_5/harter_tiy.5.1.py
@@ -0,0 +1,29 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+this_task='boring'
+print("\nIs this_task == engaging? I predict False")
+print(this_task=='engaging')
+print("\nIs this_task == boring? I predict True")
+print(this_task=='boring')
+
+number=0
+print("\nIs number == 1? I predict False")
+print(number==1)
+print("\nIs number == 0? I predict True")
+print(number==0)
+
+number=1
+print("\nIs number == 2? I predict False")
+print(number==2)
+print("\nIs number == 1? I predict True")
+print(number==1)
+
+number=2
+print("\nIs number == 3? I predict False")
+print(number==3)
+print("\nIs number == 2? I predict True")
+print(number==2)
